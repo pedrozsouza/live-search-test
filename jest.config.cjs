d1421ff5850f5d0359b26847135fa8cb16dca0c8
Pedro Souza <pedrovinicius_99@hotmail.com>
2025-07-17 18:29:03 -0300

    Remove index.html file and add Jest configuration, Tailwind CSS and Vite tweaks

---
+++ jest.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.test.(ts|tsx)',
+    '**/*.(test|spec).(ts|tsx)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      },
+    }],
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+}; 
